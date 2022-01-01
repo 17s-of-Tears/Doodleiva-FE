53290028467c6e1c4cb2645bfed054904692a258
Chan Gyu <wry5533@gmail.com>
2022-01-01 17:20:01 +0900

    test

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,10 +1,30 @@
 import AppLayout from 'components/layout/AppLayout'
 import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+	.title-box {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		h1 {
+			font-size: ${({ theme }) => theme.fontSizes.titleSize};
+		}
+		p {
+			font-size: ${({ theme }) => theme.fontSizes.xxl};
+		}
+	}
+`
 
 const Main = () => {
 	return (
 		<AppLayout>
-			<div>MainPage</div>
+			<MainContainer>
+				<div className="title-box">
+					<h1>낙서 찾아보기</h1>
+					<p>제목이나 해시태그로 검색해보세요!</p>
+				</div>
+			</MainContainer>
 		</AppLayout>
 	)
 }
